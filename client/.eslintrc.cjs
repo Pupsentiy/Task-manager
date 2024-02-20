c304c0ea36a2af7c9ad638d8cf84916311e1e81d
Pupsentiy <pupsentiy@gmail.com>
2024-02-20 12:19:19 +0300

    edit rules, added prettier yup  @hookform/resolvers react-hook-form

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,33 +1,46 @@
 module.exports = {
-    root: true,
-    env: {browser: true, es2020: true},
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:react-hooks/recommended',
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    'react-refresh',
+    'react',
+    'react-hooks',
+    'prettier',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'prettier/prettier': [
+      'warn',
+      {
+        endOfLine: 'auto',
+        singleQuote: true,
+        trailingComma: 'all',
+      },
     ],
-    ignorePatterns: ['dist', '.eslintrc.cjs'],
-    parser: '@typescript-eslint/parser',
-    plugins: [
-        'react-refresh',
-        "react",
-        "react-hooks",
-        "@typescript-eslint"],
-    rules: {
-        'react-hooks/rules-of-hooks': 'error',
-        'react-hooks/exhaustive-deps': 'error',
-        'react-refresh/only-export-components': [
-            'warn',
-            {allowConstantExport: true},
-        ],
-        "max-len": [
-            "error",
-            {
-                code: 100,
-                ignoreComments: true,
-            },
-        ],
-        "@typescript-eslint/no-explicit-any": "warn"
+    'no-var': 'error',
+    'prefer-const': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'error',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'max-len': [
+      'error',
+      {
+        code: 100,
+        ignoreComments: true,
+      },
+    ],
+    '@typescript-eslint/no-explicit-any': 'warn',
 
-    },
-}
+  },
+};
